c597696516a924d7fb863ceb3d65ed12a7d38007
Bijay Sharma <yoursbijay@outlook.com>
2023-02-01 16:18:36 +0530

    Cash Entry

---
+++ src/components/screens/CashEntry/CashEntryModal/index.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import { Modal, Form, Input, Select, notification, Descriptions } from 'antd';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+const CashEntryModal = ({visible, setVisible, callback, selectedFee, currentStudentData}) => {
+  const location = useLocation();
+  const formRef = React.useRef();
+  const [notificationApi, contextHolder] = notification.useNotification();
+  const [cashEntryLoading, setCashEntryLoading] = React.useState(false);
+  const layout = {
+    labelCol: {
+      span: 24,
+    },
+    wrapperCol: {
+      span: 24,
+    },
+  };
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
+  useEffect(() => {}, [selectedFee, currentStudentData]);
+
+  const createCashEntry = async () => {
+    const payload = {
+      student_id: currentStudentData.id,
+      fee_id: selectedFee.id,
+      collected_by:  userInfo.user_id,
+    }
+
+    try {
+      setCashEntryLoading(true);
+     await axios.post('/api/payments/cashEntry', payload);
+      notificationApi.success({
+        message: 'Cash Entry Created',
+        description: 'Cash Entry Created Successfully',
+      });
+     callback();
+     setVisible(false);
+    } catch (error) {
+      notificationApi.error({
+        message: 'Failed To Cash Entry',
+        description: error.response.data.message,
+      });
+    }finally{
+      setCashEntryLoading(false);
+    }
+  }
+
+  return ( 
+
+    <Modal style={{top: 20}} title="Confirm Cash Entry"
+      okButtonProps={{ loading: cashEntryLoading }} onOk={createCashEntry}
+    okText="Confirm"  open={visible} onCancel={() => setVisible(false)}>
+      {contextHolder}
+      <Descriptions bordered column={1}>
+        <Descriptions.Item label="Student Name">{currentStudentData?.name}</Descriptions.Item>
+        <Descriptions.Item label="Roll Number">{currentStudentData?.roll_number}</Descriptions.Item>
+        <Descriptions.Item label="Fee Name">{selectedFee?.name}</Descriptions.Item>
+        <Descriptions.Item label="Amount">{selectedFee?.amount}</Descriptions.Item>
+        <Descriptions.Item label="Payment Mode">Cash</Descriptions.Item>
+        <Descriptions.Item label="Collected By">{userInfo.name}</Descriptions.Item>
+      </Descriptions>
+      <p>
+        I acknowladge that the above mentioned amount has been collected from the student by me. 
+        </p>
+    </Modal>
+  );
+};
+
+export default CashEntryModal;
--- src/components/screens/CashEntry/index.js
+++ src/components/screens/CashEntry/index.js
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 import './style.css'
-import {  message, Table,Descriptions,Divider } from 'antd';
+import {  message, Table,Descriptions,AutoComplete, Input, Empty, Button, Tag, Space, Tooltip } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileDownload, faHistory, faIndianRupeeSign, faEye } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { SearchOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import moment from 'moment';
+import CashEntryModal from './CashEntryModal';
 
 
 const CashEntry = () => {
@@ -18,44 +24,129 @@ const CashEntry = () => {
     }
   }, [userInfo])
   const [messageApi, contextHolder] = message.useMessage()
-      
-  const columns = [
 
+  const columns = [
     {
       title: 'Fee ID',
       dataIndex: 'id',
       key: 'id',
-      sorter: (a, b) => a.id - b.id,
     },
     {
       title: 'Fee Name',
       dataIndex: 'name',
       key: 'name',
-      render: (name, record) => <a >{name}</a>,
-      sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (name, row) => <><Button type="link">{name}</Button></>
     },
-    
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+      render: (amount) => <><FontAwesomeIcon icon={faIndianRupeeSign} /> {amount}</>
+    },
     {
       title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-      filters: [
-        {
-          text: 'Active',
-          value: 'ACTIVE',
-        },
-        {
-          text: 'Standby',
-          value: 'CREATED',
-        },
-    ]
+      dataIndex: 'payment_status',
+      key: 'payment_status',
+      render: (status, row) => {
+        if (moment(row.due_date) < moment() && status !== 'captured')
+          return <Tag color="red">Overdue</Tag>;
+      
+        switch (status) {
+          case 'captured':
+            return <Tag color="green">PAID</Tag>;
+          case 'authorized':
+            return <Tag color="blue">PENDING</Tag>;
+          default:
+            return <Tag color="red">UN PAID</Tag>;
+        }
+      }
+    },
+    {
+      title: 'Due Date',
+      dataIndex: 'due_date',
+      key: 'due_date',
+      render: (dueDate) => moment(dueDate).format('Do MMMM, YYYY')
+    },
+    {
+      title: 'Payment Date',
+      dataIndex: 'payment_date',
+      key: 'payment_date  ',
+      render: (paymentDate) => paymentDate ? moment(paymentDate).format('Do MMMM, YYYY') : '-'
     },
     {
       title: 'Actions',
       dataIndex: 'actions',
       key: 'actions',
+      render: (actions, row) => {
+        if (row.payment_status !== 'captured'){
+          return <Button onClick={() => {
+            setSelectedFee(row)
+            setCashEntryModalVisible(true)
+          }}>Cash Entry</Button>
+        }
+        return (
+          <Space>
+            <Tooltip title="View Details">
+              <Button onClick={() => navigate(`/payment/${row.id}`)} shape='circle' icon={<FontAwesomeIcon icon={faEye} />} />
+            </Tooltip>
+            <Tooltip title="Download Reciept">
+              <Button onClick={() => navigate(`/receipt/${row.id}`)} shape='circle' icon={<FontAwesomeIcon icon={faFileDownload} />}  />
+            </Tooltip>
+          </Space>
+        );
+      }
     }
-  ]
+  ];
+
+  const [data, setData] = React.useState([]);
+  const [searchOPtions, setSearchOptions] = React.useState([]);
+  const [currentStudent, setCurrentStudent] = React.useState({});
+  const [currentStudentData, setCurrentStudentData] = React.useState(null);
+  const [feeDetails, setFeeDetails] = React.useState([]);
+  const [feeDetailsLoading, setFeeDetailsLoading] = React.useState(false);
+  const [selectedFee, setSelectedFee] = React.useState(null);
+
+  // Modal
+  const [CashEntryModalVisible, setCashEntryModalVisible] = React.useState(false);
+  
+  const CashEntryCallback = (value) => {
+    console.log(value)
+  }
+
+  const handleSearch = async (value) => {
+    if(value.length < 3) return;
+    try {
+      const res = await axios.post('/api/students/search', { searchText: value })
+      setData(res.data)
+      setSearchOptions(res.data.map((item) => ({ value: item.roll_number + ' - ' + item.name, label: item.roll_number + ' - ' + item.name })))
+    } catch (error) {
+      messageApi.error(error.response.data.message)
+    }
+  }
+
+  const fetchFeeReport = async (student_id) => {
+    try {
+      setFeeDetailsLoading(true);
+      const res = await axios.get(`/api/fees/student/${student_id}`);
+      if (res.status >= 200 && res.status < 400) {
+        setFeeDetails(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+      messageApi.error('Error fetching fee details');
+    } finally {
+      setFeeDetailsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (currentStudent?.length > 0) {
+      let roll = currentStudent.split('-')[0].trim()
+      let student = data.find((item) => item.roll_number === roll)
+      setCurrentStudentData(student)
+      fetchFeeReport(student.id)
+    }
+  }, [currentStudent])
 
   return (
     <>
@@ -64,19 +155,48 @@ const CashEntry = () => {
       <div className='page-header'>
         <h2>Cash Entry</h2>
       </div>
+      <AutoComplete 
+        maxLength={10}
+        style={{ width: 400 }} 
+        options={searchOPtions}
+        onSelect={(value) => setCurrentStudent(value)}
+        onSearch={value => handleSearch(value)}>
+        <Input.Search size="large" placeholder="Enter Roll Number Or Name" enterButton />
+        </AutoComplete>
       <div className="batch-list">
-        <Descriptions title="Student Details">
-          <Descriptions.Item label="Student Name">Sefali Basnet</Descriptions.Item>
-          <Descriptions.Item label="Batch Name">19CSEC</Descriptions.Item>
-          <Descriptions.Item label="Department">Computer Engineering</Descriptions.Item>
-          <Descriptions.Item label="Mobile Number">8900089820</Descriptions.Item>
-          <Descriptions.Item label="Gender">female</Descriptions.Item>
-          <Descriptions.Item label="Roll No">19CSEC04</Descriptions.Item>
-        </Descriptions>
-        <Table columns={columns}  />
+        {currentStudentData ? <>
+          <Descriptions title="Student Details">
+            <Descriptions.Item label="Student Name">{currentStudentData.name}</Descriptions.Item>
+            <Descriptions.Item label="Batch Name">{currentStudentData.batch_name}</Descriptions.Item>
+            <Descriptions.Item label="Department ID">{currentStudentData.batch_id}</Descriptions.Item>
+            <Descriptions.Item label="Mobile Number">{currentStudentData.phone_number}</Descriptions.Item>
+            <Descriptions.Item label="Email">{currentStudentData.email}</Descriptions.Item>
+            <Descriptions.Item label="Roll No">{currentStudentData.roll_number}</Descriptions.Item>
+          </Descriptions>
+          <Table columns={columns} loading={feeDetailsLoading} dataSource={feeDetails && feeDetails}  />
+        </> : <>
+          <Empty 
+            style={{
+              marginTop: '8em'
+            }}
+            image={Empty.PRESENTED_IMAGE_DEFAULT}
+            description={
+              <span>
+                Please Search & Select a Student
+              </span>
+            }
+          />
+        </>}
+        {}
+        
       </div>
     </div>
-    
+      <CashEntryModal
+       selectedFee={selectedFee}
+       currentStudentData={currentStudentData} 
+       visible={CashEntryModalVisible} 
+       setVisible={setCashEntryModalVisible} 
+       callback={CashEntryCallback} />
     </>
     
   )
